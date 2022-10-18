8c899d0e3495dabd6d780d6920ee5ece8537c279
Kasl0 <slonieckacper@gmail.com>
2022-10-18 18:14:20 +0200

    Cwiczenia 2

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -4,6 +4,12 @@ public class World {
     public static void main (String[] args) {
         System.out.println("system wystartował");
 
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+
         //args = new String[ ]{"f", "f", "r", "l"};
         Direction[] directions = StringsToDirections(args);
         run(directions);
